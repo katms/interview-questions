676c699d6ba5b128d3eb5a89a5f3e2c58b1faa78
Katherine Sullivan <katms729@gmail.com>
2015-03-22 21:51:02 -0400

    Works for one value of offset

--- Rotated.scala
+++ Rotated.scala
@@ -1,11 +1,28 @@
 object Rotated {
 	def main(args: Array[String]): Unit = {
 		val a = Vector(1,2,3,4)
-		val b = Vector(2,3,4,1)
-	  	println(is_rotated(a,b))
-	}
+		val b = Vector(3,4,1,2)
+	  	println(is_rotated(a,b)) //true
+	  	
+	  	val c = Vector(0,1,4,5) //false
+	  	val d = Vector(2,3,4,1) //rotated 1
+	  	
+	  	println(is_rotated(a,c))
+	}		
+		
 	
 	def is_rotated(a: Vector[Int], b: Vector[Int]) = {
-		false
+		def try_offset(offset: Int): Boolean = {
+			
+			def check_index(index: Int): Boolean = {
+				if(index >= a.length) true
+				else {
+					val equals = a(index) == b((index+offset) % a.length)
+					equals && check_index(index+1)
+				}
+			}
+			check_index(0)
+		}		
+		try_offset(2)
 	}
 }
