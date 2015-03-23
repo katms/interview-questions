b989c782649ebb2a6de10fb2fcca8cb9968c952e
Katherine Sullivan <katms729@gmail.com>
2015-03-22 21:32:28 -0400

    Initial commit

---
+++ Rotated.scala
@@ -0,0 +1,11 @@
+object Rotated {
+	def main(args: Array[String]): Unit = {
+		val a = Vector(1,2,3,4)
+		val b = Vector(2,3,4,1)
+	  	println(is_rotated(a,b))
+	}
+	
+	def is_rotated(a: Vector[Int], b: Vector[Int]) = {
+		false
+	}
+}
