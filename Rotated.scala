48f083eb557433e7bd0d2de44d4ed8851dca9240
Katherine Sullivan <katms729@gmail.com>
2015-03-24 01:16:52 -0400

    lambdas

--- Rotated.scala
+++ Rotated.scala
@@ -2,30 +2,30 @@ object Rotated {
 	def main(args: Array[String]): Unit = {
 		val a = Vector(1,2,3,4)
 		val b = Vector(3,4,1,2)
+	  	val c = Vector(0,1,4,5)
+	  	val d = Vector(2,3,4,1)
+	  	val e = Vector(2,3)
+	  	
 	  	println(is_rotated(a,b)) //true
-	  	
-	  	val c = Vector(0,1,4,5) //false
-	  	val d = Vector(2,3,4,1) //true
-	  	
-	  	println(is_rotated(a,c))
-	  	println(is_rotated(a,d))
+	  	println(is_rotated(a,c)) //false
+	  	println(is_rotated(a,d)) //true
+	  	println(is_rotated(a,e)) //false
 	}		
 		
 	
-	def is_rotated(a: Vector[Int], b: Vector[Int]) = {
-		//try one offset on all items, then increment
-		def try_offset(offset: Int): Boolean = {
-			//check one index and increment
-			def check_index(index: Int): Boolean = {
-				if(index >= a.length) true
-				else {
-					val equals = a(index) == b((index+offset) % a.length)
-					equals && check_index(index+1)
-				}
+	def is_rotated[T](a: IndexedSeq[Int], b: IndexedSeq[Int]) = {
+		def iterate(combine: (Boolean, Boolean) => Boolean, zero: Boolean)(fn: Int => Boolean, limit: Int => Boolean) = {
+			def iter(n: Int): Boolean = {
+				if(limit(n)) zero
+				else combine(fn(n), iter(n+1))
 			}
-			if(offset < a.length) check_index(0) || try_offset(offset+1)
-			else false //checked all possible offsets
+			iter(0)
 		}
-		try_offset(1)
+		
+		def limit(n: Int) = n >= a.length
+		
+		def check_all(offset: Int): Boolean = iterate(_ && _, true)(i => a(i) == b((i+offset) % b.length), limit)
+		
+		iterate(_ || _, false)((check_all(_)), limit)
 	}
 }
