5403a702a6babff76a66b045da5d67fe6141ac4f
Katherine Sullivan <katms729@gmail.com>
2015-03-27 03:11:59 -0400

    exists and forall

--- Rotated.scala
+++ Rotated.scala
@@ -10,22 +10,12 @@ object Rotated {
 	  	println(is_rotated(a,c)) //false
 	  	println(is_rotated(a,d)) //true
 	  	println(is_rotated(a,e)) //false
-	}		
+	  	
+	  	println(is_rotated(Vector(1,2,1,2), Vector(2,1))) //false
+	}
 		
-	
-	def is_rotated[T](a: IndexedSeq[Int], b: IndexedSeq[Int]) = {
-		def iterate(combine: (Boolean, Boolean) => Boolean, zero: Boolean)(fn: Int => Boolean, limit: Int => Boolean) = {
-			def iter(n: Int): Boolean = {
-				if(limit(n)) zero
-				else combine(fn(n), iter(n+1))
-			}
-			iter(0)
-		}
-		
-		def limit(n: Int) = n >= a.length
-		
-		def check_all(offset: Int): Boolean = iterate(_ && _, true)(i => a(i) == b((i+offset) % b.length), limit)
-		
-		iterate(_ || _, false)((check_all(_)), limit)
+	//returns true if b is a rotation of a
+	def is_rotated[T](a: IndexedSeq[Int], b: IndexedSeq[Int]): Boolean = {
+		(a.length == b.length) && ((0 until a.length) exists (o => (0 until a.length) forall (i => a(i) == b((i+o) % b.length))))
 	}
 }
